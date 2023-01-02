07a3dc11964a7ae0f1291faf80bf587dbe1fb1d7
prashant <prashant@erpnext>
2023-01-02 11:36:15 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## ERP Custom
+
+This app All Default site Customization incorporate for easy to use.
+
+#### License
+
+MIT
---
+++ erp_custom/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "ERP Custom",
+			"type": "module",
+			"label": _("ERP Custom")
+		}
+	]
